669e83e562eaf5f6d41e1d2424fdbffa79a350b2
accarras <31301740+accarras@users.noreply.github.com>
2017-11-06 18:16:49 -0500

    Add files via upload
    I did it!

--- exercise/wk3/dbPractice1.c
+++ exercise/wk3/dbPractice1.c
@@ -1,29 +1,37 @@
 //factorial of a number is the product of the number with all
 // the numbers below it.
 //eg: Factorial of 5 = 5 * 4 * 3 * 2 * 1 = 120
+#include <stdio.h>
 
 int values[1];
 int factorial(int);
 
 int main(void)
 {
-        using namespace std;
+    int n = 0; // this variable was undeclared, but needed
+               // stores the number we're working with
+        
     printf( "Debugging Practice 1 - Quiz 3, Q3\n\n");
-        printf( "Please enter the number whose factorial you wish to find: ");
-        scanf("%d", &n);
+     printf( "Please enter the number whose factorial you wish to find: ");
+    scanf("%d", &n);
 
-        int fact = factorial(n);
-        printf("The factorial of %f is %d", &n,factorial)
+    int fact = factorial(n);
+    printf("The factorial of %d is %d", n,fact);
 
     return 0;
-}
 }
 
 int factorial(int number)
 {
+    // this definitely only works with positive integers
+    // is fine for the scope of this debugging?
     int factorial = 1;
-    for (int i =1; i<=num; i--)
-            factorial =* i;
+    for (int i=1; i<=number; i++){
+            // factorial starts off at 1
+            // it is multiplied by 1 + the number of iterations starting at 0
+            // iterations go until it has looped as "number" times 
+            factorial = factorial * i;
+    } // tasteful brackets added
 
-    return fact;
+    return factorial;
 }
--- exercise/wk3/dbPractice2.c
+++ exercise/wk3/dbPractice2.c
@@ -1,30 +1,32 @@
-
-include#stdio.h#
+#include<stdio.h>
 
 int main(void)
 {
     int size=3, Matrix[size][size];
 
-    printf("Enter 9 elements of the matrix");
+    printf("Enter 9 elements of the matrix: \n");
 
-          for (int i=0; i<1; j++)
-            for (int j=0; j<size; j++)
-
-                scanf("%f", Matrix1[1][1]);
-
-        display(Matrix);
-    return 0;
-}
-
-float display(int Matrix1[][], int size)
-{
-    int size = 6;
-        for (int i=0; i<size; i++) {
-            for (int j=0; j<size; j++) {
-            printf("%c ,", Matrix1[i][j]);
-
-        printf("\n");
+    for (int i=0; i<size; i++){
+        for (int j=0; j<size; j++){
+            scanf("%d", &Matrix[i][j]);
         }
+    }
+    
+    printf("\n");
+    
+    display(Matrix, size);
+    
     return 0;
 }
 
+int display(int Matrix1[3][3], int size)
+{
+    for (int i=0; i<size; i++) {
+        for (int j=0; j<size; j++) {
+            printf("%d", Matrix1[i][j]);
+            printf(" ");
+        }
+    }
+    
+    return 0;
+}
